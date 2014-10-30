e96c9e0f0eb6c41eeb801b4ca3ce6aeab5a634af
Christopher Baines <cb15g11@soton.ac.uk>
2014-10-30 09:28:43 +0000

    Replace the python web server with a nodejs one
    This allows for the use of web sockets, the events then show up on the
    index.html page.

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+var bodyParser = require('body-parser')
+
+app.use(express.static(__dirname));
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json())
+
+app.options('/log', function(req, res) {
+  res.sendStatus(200);
+});
+
+var socket = null;
+
+app.ws('/', function(ws, req) {
+  socket = ws;
+});
+
+app.post('/log', function(req, res) {
+  console.log(req.body);
+  if (socket !== null) {
+    socket.send(JSON.stringify(req.body));
+  }
+
+  res.send({ status: "ok" });
+});
+
+app.listen(5000);
