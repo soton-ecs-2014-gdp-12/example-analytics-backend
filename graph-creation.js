2795eb76c675d7bb2ace41c1a538d57d5a0cf1ac
Christopher Hewett <chewett@hotmail.co.uk>
2014-11-08 20:01:52 +0000

    Added example graphs for the analytics

--- graph-creation.js
+++ graph-creation.js
@@ -1,9 +1,13 @@
+/*global d3:false */
 
 (function() {
     "use strict";
 
     createScatterTimeWatched();
     createScatterPercentWatched();
+    createBarFailedAttempts();
+    createBarAnswers();
+    createBarRewatch();
 
     function createScatterTimeWatched() {
 
@@ -94,6 +98,70 @@
             .call(yAxis);
 
 
+    }
+
+    function createBarFailedAttempts() {
+        var dataset = [{name : "0", value: 10}, {name: "1", value: 5}, {name: "2", value: 2},{name: "3", value: 0}, {name: "4", value: 1} ];
+        createBar("#svg-q1-failed-attempts", dataset);
+    }
+
+    function createBarRewatch() {
+        var dataset = [{name : "Rewatched segment", value: 25}, {name: "Continued", value: 4}];
+        createBar("#svg-q1-rewatch", dataset);
+    }
+
+    function createBarAnswers() {
+        var dataset = [{name : "cheese", value: 10}, {name: "cheeese", value: 15}, {name: "bob", value: 1}];
+        createBar("#svg-q1-answers", dataset);
+    }
+
+    function createBar(idName, dataset) {
+
+        var margin = {top: 20, right: 30, bottom: 30, left: 40},
+            width = 500 - margin.left - margin.right,
+            height = 300 - margin.top - margin.bottom;
+
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], 0.1);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
+
+        var svg = d3.select(idName)
+            .insert("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        x.domain(dataset.map(function(d) { return d.name; }));
+        y.domain([0, d3.max(dataset, function(d) { return d.value; })]);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        svg.selectAll(".bar")
+            .data(dataset)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.name); })
+            .attr("y", function(d) { return y(d.value); })
+            .attr("height", function(d) { return height - y(d.value); })
+            .attr("width", x.rangeBand());
     }
 
 })();
