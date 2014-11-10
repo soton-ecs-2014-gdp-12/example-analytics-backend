77312f925b85d83aaf68d66f58e3d15d2ca9700e
Christopher Hewett <chewett@hotmail.co.uk>
2014-11-10 19:45:09 +0000

    Fixed tab/space issue by converting all to tabs

--- server.js
+++ server.js
@@ -9,30 +9,30 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname));
 
 app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
 app.use(bodyParser.json());
 
 app.options('/log', function(req, res) {
-  res.sendStatus(200);
+	res.sendStatus(200);
 });
 
 var socket = null;
 
 app.ws('/', function(ws, req) {
-  socket = ws;
+	socket = ws;
 });
 
 app.post('/log', function(req, res) {
-  console.log(req.body);
-  if (socket !== null) {
-    socket.send(JSON.stringify(req.body));
-  }
+	console.log(req.body);
+	if (socket !== null) {
+		socket.send(JSON.stringify(req.body));
+	}
 
-  res.send({ status: "ok" });
+	res.send({ status: "ok" });
 });
 
 app.listen(5000);
